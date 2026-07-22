0c3105eedb253fef3309ca8a3cfa39f24f37f230
Arun Thakur <bedupako12mas@gmail.com>
2026-07-22 22:46:12 +0530

    Basic Getting started guide with fastify

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-const message:string = "Hello World";
-console.log(message);
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+  logger: true
+});
+
+fastify.get('/', function (request,  reply) {
+  reply.send({hello: 'world'})
+});
+
+fastify.listen({ port : 3000 } , function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+});
+
+
